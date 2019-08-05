7c616c23da4c0e2194f6f0ff8c024e508abeba8e
Serdjjio <konetsergey@gmail.com>
2019-08-06 00:09:13 +0200

    Example Read file.xml from Dom

---
+++ 2019_05_08_DOM_ReadXML_Example/src/Example1/Employee1.java
@@ -0,0 +1,18 @@
+package Example1;
+
+public class Employee1 {
+    private String name, job;
+
+    public Employee1(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+}
