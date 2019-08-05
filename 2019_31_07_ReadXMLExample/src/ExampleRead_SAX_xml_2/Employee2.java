57b22074861903b0d7047cfb570a0cf6593ee61c
Serdjjio <konetsergey@gmail.com>
2019-08-05 18:28:56 +0200

    3typeRead

---
+++ 2019_31_07_ReadXMLExample/src/ExampleRead_SAX_xml_2/Employee2.java
@@ -0,0 +1,21 @@
+package ExampleRead_SAX_xml_2;
+
+public class Employee2 {
+
+    private String name;
+    private String job;
+
+    public Employee2(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+}
